aa7ff898d516284228cebbb103d8d3c7e5fa3b1f
jackwang <469540388@qq.com>
2023-07-29 11:41:55 +0800

    [辅助变动]:更改标题，注释及跳转链接

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -448,7 +448,7 @@ export function ImagePreviewer(props: {
           <div>
             <div className={styles["main-title"]}>ChatGPT Next Web</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              github.com/wang-jiaye/ChatGPT-Next-Web
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          Jack ChatGPT 
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Learning to use tools is a hallmark of human evolution.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
